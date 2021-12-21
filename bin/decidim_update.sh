b461f122ceb97575525f4f748a612f999cb4decb
Hugoren Martinako <aumpfbahn@gmail.com>
2021-12-21 18:20:50 +0100

    Update decidim_update.sh

--- bin/decidim_update.sh
+++ bin/decidim_update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-echo "- Updating decidim...."
-bundle update
-echo "- Updating decidim webpacker config"
-bin/rails decidim:webpacker:install
-echo "- Installing decidim new migrations"
-bin/rails decidim:upgrade
-echo "- Updating application"
-bin/update
-echo "Ready! Please commit and deploy"
+# handle kill signs
+trap "echo; exit 1" INT TERM
 
+function log() {
+  echo
+  echo "=================================================="
+  echo $1
+  echo "=================================================="
+  echo
+}
+
+log "Updating decidim" && bundle update
+log "Updating decidim webpacker config" && bin/rails decidim:webpacker:install
+log "Installing decidim new migrations" && bin/rails choose_target_plugins && bin/rails railties:install:migrations
+log "Updating application" && bin/update
+log "Ready! Please commit and deploy"
