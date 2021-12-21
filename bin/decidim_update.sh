1fadda0d37e48ab46895db8f1caaa7eb9dee87c4
Fernando Blat <fernando@blat.es>
2021-12-21 10:19:50 +0100

    Update update script

--- bin/decidim_update.sh
+++ bin/decidim_update.sh
@@ -2,6 +2,8 @@
 
 echo "- Updating decidim...."
 bundle update
+echo "- Updating decidim webpacker config"
+bin/rails decidim:webpacker:install
 echo "- Installing decidim new migrations"
 bin/rails decidim:upgrade
 echo "- Updating application"
