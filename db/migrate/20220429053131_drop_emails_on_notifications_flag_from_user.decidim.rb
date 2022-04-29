4c66b34b72c83f523344b05b7fd68b9f78190996
quino <quino@populate.tools>
2022-04-29 10:30:29 +0200

    Remove dup migration

--- db/migrate/20220429053131_drop_emails_on_notifications_flag_from_user.decidim.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from decidim (originally 20220427142214)
-
-class DropEmailsOnNotificationsFlagFromUser < ActiveRecord::Migration[5.1]
-  def change
-    Decidim::User.where(email_on_notification: true).each { |user| user.update(notifications_sending_frequency: "real_time") }
-    remove_column :decidim_users, :email_on_notification
-  end
-end
